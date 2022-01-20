670ab9fa32bc55ac65627baf1eb1afbb266f2029
NexusProgrammingStudio <60765914+NexusProgrammingStudio@users.noreply.github.com>
2022-01-20 14:00:37 +0530

    Player Movement
    Player Movement(Forward, Backward, Jump, and Crouch).

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed=Input.GetAxisRaw("Horizontal");
+        float jump = Input.GetAxisRaw("Vertical");
+        m_Animator.SetFloat("Speed",Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        Vector3 pos = transform.localPosition;
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+            pos.x = -0.1f + pos.x;
+            // pos.y = 0.1f - pos.y;
+            pos.z = 0;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            scale.x = 1f * Mathf.Abs(scale.x);
+            pos.x = 0.1f + pos.x;
+            // pos.y = 0.1f + pos.y; 
+            pos.z = 0;
+              
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            m_Animator.SetTrigger("Jump_trigger");
+
+            m_Animator.ResetTrigger("Crouch_Trigger");
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            m_Animator.SetTrigger("Crouch_Trigger");
+
+            m_Animator.ResetTrigger("Jump_trigger");
+        }
+        transform.localScale = scale;
+        transform.localPosition = pos;
+    }
+}
