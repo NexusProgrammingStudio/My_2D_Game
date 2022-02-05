bcfb8bed00482b841d20c9d7eb27d16e309f4da9
NexusProgrammingStudio <60765914+NexusProgrammingStudio@users.noreply.github.com>
2022-02-05 11:45:56 +0530

    Game Lobby UI Implementation
    Lobby UI with Start Button

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    public Button buttonStart;
+
+    public void Awake()
+    {
+        buttonStart.onClick.AddListener(PlayGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
